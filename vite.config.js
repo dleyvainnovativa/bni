aeea2155fb8b7c908f229a058dd8c7ef9c7e0542
Daniel Leyva <caballerodlc@outlook.com>
2025-11-10 13:41:30 -0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js','resources/js/search.js', 'resources/css/theme.css'],
+            input: ['resources/scss/app.scss', 'resources/js/app.js','resources/js/search.js','resources/js/business.js', 'resources/css/theme.css'],
             refresh: true,
         }),
     ],
